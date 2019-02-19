b3077285de734ac62edfb489b7ec731afdb00a22
KieranVieira <kieranvieira@live.com>
2019-02-19 16:36:27 -0500

    Added post and get routes for bear router

--- routes/bear-router.js
+++ routes/bear-router.js
@@ -6,8 +6,32 @@ const router = express.Router();
 
 const db = knex(knexConfig.development)
 
+router.post('/', (req, res) => {
+    try{
+        db('bears')
+            .insert(req.body)
+            .then(id => {
+                res.status(201).json({ message: `Bear was added with Id ${id}`})
+            })
+            .catch(error => {
+                res.status(400).json({ error })
+            })
+    }catch(error){
+        res.status(500).json({
+            message: "Server could not post bear",
+            error
+        })
+    }
+})
+
 router.get('/', (req, res) => {
-    res.send('bears')
+    db('bears')
+        .then(bears => {
+            res.status(200).json(bears);
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Server could not get bear data", error })
+        })
 });
 
 module.exports = router;
