24663ca2ebc0e6dd70573149a57059f0bf0cd588
KieranVieira <kieranvieira@live.com>
2019-02-19 16:52:49 -0500

    Added delete and put requests (Stretch Completed)

--- routes/bear-router.js
+++ routes/bear-router.js
@@ -34,4 +34,66 @@ router.get('/', (req, res) => {
         })
 });
 
+router.get('/:id', (req, res) => {
+    db('bears')
+        .where({ id: req.params.id })
+        .first()
+        .then(bear => {
+            if(bear){
+                res.status(200).json(bear);
+            }else{
+                res.status(404).json({
+                    message: "Could not find bear with given ID" 
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Server could not get bear data", error })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    db('bears')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if(count){
+                res.status(204).end();
+            }else{
+                res.status(404).json({
+                    message: "Could not find bear with given ID" 
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Server could not delete bear data", error })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    try{
+        db('bears')
+            .where({ id: req.params.id })
+            .update(req.body)
+            .then(count => {
+                if(count){
+                    res.status(200).json({ message: `Bear with ID ${req.params.id} was updated to ${req.body.name}` })
+                }else{
+                    res.status(404).json({ message: `Bear could not be found with ID ${req.params.id}` })
+                }
+            })
+            .catch(error => {
+                res.status(400).json({ 
+                    message: "Bad Request, please provide a name of the bear", 
+                    error 
+                })
+            })
+    }catch(error){
+        res.status(500).json({
+            message: "Server could not update the bear",
+            error
+        })
+    }
+})
+
 module.exports = router;
