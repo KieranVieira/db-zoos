456f891eb640a1fee1d4471bc32df6f68644244a
KieranVieira <kieranvieira@live.com>
2019-02-19 16:02:15 -0500

    Added knex structure and post/get request

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile')
+
+const db = knex(knexConfig.development)
 
 const server = express();
 
@@ -7,6 +11,53 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+  try {
+    db('zoos')
+      .insert(req.body)
+      .then(item => {
+        res.status(201).json(item)
+      })
+      .catch(error => {
+        res.status(400).json({
+          message: 'Bad request, include required fields.',
+          error
+        })
+      })
+  } catch (error) {
+    res.status(500).json({ message:"Server couldn't add item", error })
+  }
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos)
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Couldn't fetch zoo data", error })
+    })
+});
+
+server.get('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .first()
+    .then(zoos => {
+      if(zoos){
+        res.status(200).json(zoos)
+      }else{
+        res.status(404).json({
+          message: "Cannot find zoo with this Id",
+        })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Couldn't fetch zoo data", error })
+    })
+});
+
+
 
 const port = 3300;
 server.listen(port, function() {
